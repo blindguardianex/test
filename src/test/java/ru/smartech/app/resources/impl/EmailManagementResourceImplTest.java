b9f3e14dab336b0b6b674a3ea0a2900a930b6b4e
Titov Dmitriy <titov.dmitrii@otr.ru>
2022-08-05 15:58:19 +0300

    implement tests for balanceManager && emailManagementResurce

--- src/test/java/ru/smartech/app/resources/impl/EmailManagementResourceImplTest.java
+++ src/test/java/ru/smartech/app/resources/impl/EmailManagementResourceImplTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import ru.smartech.app.dto.EmailDto;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @AutoConfigureMockMvc(addFilters = false)
 class EmailManagementResourceImplTest {
